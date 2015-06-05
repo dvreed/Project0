d1011205cba060c2f250c69479b6e827212aa3a1
David Reed <david.byui@gmail.com>
2015-06-05 13:55:13 -0700

    Change to dark Material Theme

--- app/src/main/java/com/udacity/dareed/project0/MainActivity.java
+++ app/src/main/java/com/udacity/dareed/project0/MainActivity.java
@@ -1,15 +1,15 @@
 package com.udacity.dareed.project0;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
